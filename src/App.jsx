1bac66a1ddd439e7047f9c56dbf3ec9aa88ecf9b
atom <vishnuramguru@gmail.com>
2025-01-20 15:40:45 +0530

    updated UI
    bugs: no mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,36 @@
 import React from "react";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  NavbarMenu,
-  NavbarMenuItem,
-  NavbarMenuToggle,
-  Link,
-  Input,
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownItem,
-  Avatar,
-} from "@nextui-org/react";
-import { SearchIcon } from "./components/SearchIcon";
+import NavbarComponent from "./components/NavbarComponent";
+import MusicPlayerCard from "./components/MusicPlayerCard";
+import PlaylistHeader from "./components/PlaylistHeader";
+import PlaylistTracks from "./components/PlaylistTracks";
 
 export default function App() {
   return (
-    <div className="flex flex-col min-h-screen dark bg-background text-foreground">
-      {/* Safe Area Wrapper */}
-      <div className="mx-auto w-full max-w-screen-2xl px-20 flex flex-col flex-1">
-        {/* Navbar */}
-        <Navbar isBordered className="bg-background/50 backdrop-blur-md">
-          <NavbarBrand>
-            <p className="font-bold text-inherit">jamify</p>
-          </NavbarBrand>
-          <NavbarContent className="items-center" justify="center">
-            <NavbarItem>
-              <Link color="foreground" href="#">
-                Punisher's Jam
-              </Link>
-            </NavbarItem>
-          </NavbarContent>
-
-          <NavbarContent as="div" className="items-center" justify="end">
-            <Dropdown placement="bottom-end">
-              <DropdownTrigger>
-                <Avatar
-                  isBordered
-                  radius="lg"
-                  as="button"
-                  className="transition-transform"
-                  color="default"
-                  size="md"
-                />
-              </DropdownTrigger>
-              <DropdownMenu aria-label="Profile Actions" variant="flat">
-                <DropdownItem key="profile" className="h-10 gap-2">
-                  <p className="font-semibold">punisher</p>
-                </DropdownItem>
-                <DropdownItem key="delete-room" color="danger">
-                  Delete Room
-                </DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-          </NavbarContent>
-        </Navbar>
-
+    <div className="flex flex-col h-screen">
+      {/* Navbar */}
+      <NavbarComponent />
+      {/* Safe Area Container */}
+      <div className="flex-1 px-0 sm:px-4 lg:px-10 overflow-hidden">
         {/* 2-Column Layout */}
-        <div className="flex flex-1">
+        <div className="flex flex-1 h-full">
           {/* Left Column (50%) */}
           <div className="w-[50%] bg-background p-4 border-r border-divider overflow-y-auto">
-            <h2 className="text-lg font-bold mb-4">Left Column</h2>
-
+            <MusicPlayerCard />
           </div>
-
           {/* Right Column (50%) */}
           <div className="w-[50%] bg-background p-4 overflow-y-auto">
-            <div>
-              <Input
-                classNames={{
-                  base: "max-w-full sm:max-w-auto h-10",
-                  mainWrapper: "h-full",
-                  input: "text-small",
-                  inputWrapper:
-                    "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-                }}
-                placeholder="Spotify track or playlist url"
-                size="sm"
-                startContent={<SearchIcon size={18} />}
-                type="search"
-              />
+            <div className="h-auto">
+              <PlaylistHeader />
+              <PlaylistTracks />
+              <PlaylistTracks />
+              <PlaylistTracks />
+              <PlaylistTracks />
+              <PlaylistTracks />
+              <PlaylistTracks />
+              <PlaylistTracks />
+              <PlaylistTracks />
+              <PlaylistTracks />
+              <PlaylistTracks />
             </div>
           </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {NextUIProvider} from "@nextui-org/react";
+import {HeroUIProvider} from "@heroui/react";
 import './index.css'
 import App from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <NextUIProvider>
-        <App />
-    </NextUIProvider>
+    <HeroUIProvider>
+      <main className="dark text-foreground bg-background">
+          <App />
+      </main>
+    </HeroUIProvider>
   </StrictMode>,
 )
